a630601747fd5ef7784b900bd6ebcdf8ca506848
Chernovi <deuse27@gmail.com>
2024-07-15 13:49:24 +0300

    Complete

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,23 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarServiceImpl;
+
+@Controller
+
+public class CarController {
+    private final CarServiceImpl carServiceImpl;
+
+    public CarController(CarServiceImpl carServiceImpl) {
+        this.carServiceImpl = carServiceImpl;
+    }
+
+    @GetMapping("/cars")
+    public String index(@RequestParam(defaultValue="5") Integer count, ModelMap model) {
+        model.addAttribute("cars", carServiceImpl.getCars(count));
+        return "cars";
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,22 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarServiceImpl implements CarService {
+    @Override
+    public List<Car> getCars(Integer count) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(1,"m1",123));
+        cars.add(new Car(2,"m2",456));
+        cars.add(new Car(3,"m3",789));
+        cars.add(new Car(4,"m4",235));
+        cars.add(new Car(5,"m5",134));
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+}
